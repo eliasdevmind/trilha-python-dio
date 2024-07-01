50ec2f4cf2f4be9c864cdc7a442dfaf445638563
eliasdevmin <eliasaraujo1222@outlook.com>
2024-07-01 00:56:45 -0300

    ESTUDO

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -1,14 +1,29 @@
-frutas = ["laranja", "maca", "uva"]
-print(frutas)
+#frutas = ["laranja", "maca", "uva"]
+#print(frutas)
+
+frutas = ['laranja', 'maça', 'uva']
+print(frutas[0])
+
+
+#frutas = []
+#print(frutas)
 
 frutas = []
 print(frutas)
 
-letras = list("python")
+
+#letras = list("python")
+#print(letras)
+
+letras = list('Python')
 print(letras)
+
+#numeros = list(range(10))
+#print(numeros)
 
 numeros = list(range(10))
 print(numeros)
 
-carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
-print(carro)
+
+#carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+#print(carro)
--- 01 - Estrutura de dados/01 - Listas/09_copy.py
+++ 01 - Estrutura de dados/01 - Listas/09_copy.py
@@ -1,5 +1,11 @@
 lista = [1, "Python", [40, 30, 20]]
 
-lista.copy()
+l2 = lista.copy()
 
 print(lista)  # [1, "Python", [40, 30, 20]]
+
+l2.append('Elias')
+
+print(l2)
+
+print(id(l2), id(lista))
